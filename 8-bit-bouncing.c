1dd6afc74fbceea22de5d5c28e989cbad3bd4612
Deborahganta <deborahganta98@gmail.com>
2022-10-06 09:20:05 +0530

    8051 leds bouncing

---
+++ 8-bit-bouncing.c
@@ -0,0 +1,112 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2022-09-30, 15:06, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "Bits1.h"
+#include "BitsIoLdd1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+void lldd_ca_cc_leds(unsigned char i);
+void lldd_ca_cc_leds(unsigned char i)
+{
+	unsigned char ca , cc;
+	ca=i;
+	ca=~ca;
+	ca=ca&0x0f; //ca=ca&0xf0;
+	
+	cc=i&0xf0; // cc=i&0x0f;
+	i=ca|cc;
+	Bits1_PutVal(i);
+}
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+int z;
+unsigned char i;
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  while(1)
+  {
+	  unsigned char m;
+	  for(m=0x80; m>0;m=m>>1)
+	  {
+	  for(i=m;i>0;i=i>>1)
+	  {
+		  lldd_ca_cc_leds(i);
+		  for(z=0;z<0x100fff;z++);
+	  }	
+	  
+	  for(i=01;i<m;i=i<<1)
+	  {
+	   lldd_ca_cc_leds(i);
+	  for(z=0;z<0x100fff;z++);		  
+	  }	
+	  
+	  
+	  
+  }
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
