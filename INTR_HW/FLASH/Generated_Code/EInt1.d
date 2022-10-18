64354c6375aacf19f94b1f6b743ff903c0ad9f8a
Deborahganta <deborahganta98@gmail.com>
2022-10-18 14:26:59 +0530

    Example for Interrupt occurance

---
+++ INTR_HW/FLASH/Generated_Code/EInt1.d
@@ -0,0 +1,103 @@
+Generated_Code/EInt1.o: ../Generated_Code/EInt1.c \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Sources/Events.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Error.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Const.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/IO_Map.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Red.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Types.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Error.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Const.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/IO_Map.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd1.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Cpu.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd1.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Blue.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd2.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd2.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/EInt1.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/ExtIntLdd1.h \
+ C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h \
+ C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/ExtIntLdd1.h \
+ ../Generated_Code/EInt1.h
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Sources/Events.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Error.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Const.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/IO_Map.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Red.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Types.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Error.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/PE_Const.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/IO_Map.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd1.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Cpu.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd1.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/Blue.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd2.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/BitIoLdd2.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/EInt1.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/ExtIntLdd1.h:
+
+C:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h:
+
+C:/Users/XDT1KOR/workspace/INTR_HW/Generated_Code/ExtIntLdd1.h:
+
+../Generated_Code/EInt1.h:
---
+++ INTR_HW/Sources/Events.c
@@ -0,0 +1,96 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2022-10-18, 14:08, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  EInt1_OnInterrupt (module Events)
+**
+**     Component   :  EInt1 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void EInt1_OnInterrupt(void)
+{
+	Red_PutVal(1);
+	int j;
+  /* Write your code here ... */
+	Blue_PutVal(0);
+	for(j=0; j<0x100fff; j++);
+	Blue_PutVal(1);
+}
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
